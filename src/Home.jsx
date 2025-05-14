dc6855d1c5b302f5e078bf0b34620030fcd37c9e
Manjot Kaur <mk6403730@gmail.com>
2025-05-14 16:16:16 +0530

    updated config

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+// // Lecture-57 [Setup react router 7]
+// function Home() {
+//     return (
+//       <>
+//         <h1>Home Page</h1>
+//       </>
+//     )
+//   }
+
+// // Lecture-58
+function Home() {
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  );
+}
+export default Home;
