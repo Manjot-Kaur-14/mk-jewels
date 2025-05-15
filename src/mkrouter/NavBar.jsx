78d0292e2c363bdcb421c947adcad77dc666f1a3
Manjot Kaur <mk6403730@gmail.com>
2025-05-15 12:37:27 +0530

    updated

--- src/mkrouter/NavBar.jsx
+++ src/mkrouter/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     <div class="top-row">
       <div class="logo">
         
-        <img src="/images/mklogo.png" alt="MK Jewels Logo" />
+        <img src="images/mklogo.png" alt="MK Jewels Logo" />
       </div>
       <div class="search-bar">
         <input
@@ -30,48 +30,48 @@ function NavBar() {
     {/* <!-- Bottom Navigation --> */}
     <nav class="bottom-nav">
       <NavLink className={({isActive})=> isActive?'custom-active link':'link'} to="/">
-            <img src="/images/jewellery.svg" alt="All Jewellery" />
+            <img src="images/jewellery.svg" alt="All Jewellery" />
         <span>All Jewellery</span>
           </NavLink>
       {/* <a href="index.html">
-        <img src="/images/jewellery.svg" alt="All Jewellery" />
+        <img src="images/jewellery.svg" alt="All Jewellery" />
         <span>All Jewellery</span>
       </a> */}
       <NavLink className={({isActive})=> isActive?'custom-active link':'link'} to="/gold">
-            <img src="/images/gold.svg" alt="Gold" />
+            <img src="images/gold.svg" alt="Gold" />
         <span>Gold</span>
           </NavLink>
      
       <a href="diamond.html">
-        <img src="/images/diamond.svg" alt="Diamond" />
+        <img src="images/diamond.svg" alt="Diamond" />
         <span>Diamond</span>
       </a>
       <a href="earrings.html">
-        <img src="/images/earrings.svg" alt="Earrings" />
+        <img src="images/earrings.svg" alt="Earrings" />
         <span>Earrings</span>
       </a>
       <a href="rings.html">
-        <img src="/images/rings.svg" alt="Rings" />
+        <img src="images/rings.svg" alt="Rings" />
         <span>Rings</span>
       </a>
       <a href="dailywear.html">
-        <img src="/images/dailywear.svg" alt="Daily Wear" />
+        <img src="images/dailywear.svg" alt="Daily Wear" />
         <span>Daily Wear</span>
       </a>
       <a href="collection.html">
-        <img src="/images/collection.svg" alt="Collections" />
+        <img src="images/collection.svg" alt="Collections" />
         <span>Collections</span>
       </a>
       <a href="wedding.html">
-        <img src="/images/wedding.svg" alt="Wedding" />
+        <img src="images/wedding.svg" alt="Wedding" />
         <span>Wedding</span>
       </a>
       <a href="gifting.html">
-        <img src="/images/gifting.svg" alt="Gifting" />
+        <img src="images/gifting.svg" alt="Gifting" />
         <span>Gifting</span>
       </a>
       <a href="more.html">
-        <img src="/images/more.svg" alt="More" />
+        <img src="images/more.svg" alt="More" />
         <span>More</span>
       </a>
     </nav>
